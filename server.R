bc479d2a789d92b9e1eb48fdc5ae8ccbae8938df
Bolz, Sebastian <sebastian.bolz@immobilienscout24.de>
2014-12-04 14:16:54 +0100

    added plot to shiny app

--- server.R
+++ server.R
@@ -1,6 +1,13 @@
 library(shiny)
+library(ggplot2)
+
+data(father.son)
 
 shinyServer(
     function(input, output) {
+        output$plot <- renderPlot({
+            g <- ggplot(father.son, aes(sheight, fheight))
+            g + geom_point() + geom_smooth(method = "lm", formula = y ~ x)
+        })
     }
 )
--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@ shinyUI(pageWithSidebar(
         h3('Sidebar text')
     ),
     mainPanel(
-        h3('Main Panel text')
+        h3('Main Panel text'),
+        plotOutput('plot')
     )
 ))
