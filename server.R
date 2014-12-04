ccad0654a04b4935ef4e442bc0c476e6c80d0b18
Bolz, Sebastian <sebastian.bolz@immobilienscout24.de>
2014-12-04 22:40:29 +0100

    refactored to use glm instead of lm

--- server.R
+++ server.R
@@ -2,20 +2,26 @@ library(shiny)
 
 data(swiss)
 
-fitLinearModel <- function(predictor) {
-    f <- paste(names(swiss)[6], '~', predictor)
-    lm(formula = f, data = swiss)
+fitLinearModel <- function(predictors) {
+    f <- paste(names(swiss)[6], '~', paste(predictors, collapse = ' + '))
+    glm(formula = f, data = swiss)
+}
+
+plotModel <- function(fit, n) {
+    plot(fit, which = n)
+}
+
+summarizeModel <- function(fit) {
+    summary(fit)
 }
 
 shinyServer(
     function(input, output) {
-        output$predictor <- renderText(input$predictor)
-        fit <- reactive(fitLinearModel(input$predictor))
-        output$fit <- renderTable(fit())
-        output$plot1 <- renderPlot(plot(fit(), which = 1))
-        output$plot2 <- renderPlot(plot(fit(), which = 2))
-        output$plot3 <- renderPlot(plot(fit(), which = 3))
-        output$plot4 <- renderPlot(plot(fit(), which = 4))
-        output$data <- renderTable(swiss)
+        output$data <- renderTable(head(swiss, 7))        
+        fit <- reactive(fitLinearModel(input$predictors))
+        output$fit <- renderPrint(summarizeModel(fit()))
+        output$plot1 <- renderPlot(plotModel(fit(), 1))
+        output$plot2 <- renderPlot(plotModel(fit(), 2))
+        output$plot3 <- renderPlot(plotModel(fit(), 3))
     }
 )
--- ui.R
+++ ui.R
@@ -2,17 +2,62 @@ library(shiny)
 
 data(swiss)
 
-shinyUI(pageWithSidebar(
-    headerPanel("Linear Regression on Swiss Data Set"),
-    sidebarPanel(
-        radioButtons('predictor', 'Choose Predictor:', names(swiss)[-6])
-    ),
-    mainPanel(
-        tableOutput('data'),        
-        tableOutput('fit'),
-        plotOutput('plot1'),
-        plotOutput('plot2'),
-        plotOutput('plot3'),
-        plotOutput('plot4')
+shinyUI (
+    fluidPage (
+        title = "Linear Regression on Swiss Data Set",
+        h1("Linear Regression on Swiss Data Set"),
+        fluidRow (
+            column (
+                5,
+                h3('Data Set Sample'),
+                p (
+                    paste (
+                        'The swiss data set contains a standardized fertility measure',
+                        'and socio-economic indicators for each of 47 French-speaking',
+                        'provinces of Switzerland at about 1888 as well as the infant',
+                        'mortality rate of these provinces. All variables but the',
+                        'fertility give the proportions of the population.'
+                    )
+                ),
+                tableOutput('data'),
+                p (
+                    paste (
+                        'Please choose at least one predictor with which you want',
+                        'to predict the infant mortality rate. With your selection',
+                        'a generalized linear model is fitted whose summary is',
+                        'displayed to the right. The model fit is also visualized',
+                        'in three different plots which you can find below.'
+                    )
+                )
+            ),
+            column (
+                5,
+                offset = 1,
+                verbatimTextOutput('fit')
+            )
+        ),
+        fluidRow (
+            column (
+                2,
+                checkboxGroupInput (
+                    "predictors",
+                    "Choose predictor(s):",
+                    names(swiss)[-6],
+                    names(swiss)[1]
+                )
+            ),
+            column (
+                3,
+                plotOutput('plot1')
+            ),
+            column (
+                3,
+                plotOutput('plot2')
+            ),
+            column (
+                3,
+                plotOutput('plot3')
+            )
+        )
     )
-))
+)
